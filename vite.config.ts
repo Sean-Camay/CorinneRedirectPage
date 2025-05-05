d2718f03568b0df61ad903d408aa108a851afe64
Sean Unland <sdunland@gmail.com>
2025-05-05 18:17:20 -0400

    add production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CorinneRedirectPage/', // ensures all asset URLs start with /CorinneRedirectPage/
   plugins: [react(), tailwindcss()],
-});
+  build: {
+    outDir: 'docs', // write your built site into “docs/”
+  },
+})
