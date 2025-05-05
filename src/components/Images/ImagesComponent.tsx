91bebfe624733438a1a2958f6693e20a3e6734e0
Sean Unland <sdunland@gmail.com>
2025-05-05 18:53:48 -0400

    moved images to src/assets

--- src/components/Images/ImagesComponent.tsx
+++ src/components/Images/ImagesComponent.tsx
@@ -1,20 +1,15 @@
+import CorinneHay from '../../assets/corinne-hay.png'
+import CorinneCamay from '../../assets/corinne-camay.png'
+
 export const ImagesComponent = () => {
   return (
     <>
       <div className='flex flex-row items-center justify-center'>
-        <img
-          src='../../../public/corinne-hay.png'
-          alt='corinne-hay'
-          className='h-36 w-64'
-        />
+        <img src={CorinneHay} alt='corinne-hay' className='h-36 w-64' />
 
         <h2 className='text-black'>to</h2>
 
-        <img
-          src='../../../public/corinne-camay.png'
-          alt='corinne-camay'
-          className='h-36 w-64'
-        />
+        <img src={CorinneCamay} alt='corinne-camay' className='h-36 w-64' />
       </div>
     </>
   )
