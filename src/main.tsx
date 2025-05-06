02437d6d2fd9f19b5f4b1451eca8d99f00f4b101
Sean Unland <sdunland@gmail.com>
2025-05-05 20:50:12 -0400

    updated BrowserRouter basename from CorinneRedirectPage to / because it needs to match the base in vite.config.ts

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/CorinneRedirectPage/'>
+    <BrowserRouter basename='/'>
       <StrictMode>
         <App />
       </StrictMode>
