89b84cc0dc74c83e22181d56f18cb9f6260cfa5b
Sean Unland <sdunland@gmail.com>
2025-05-05 20:12:08 -0400

    changed base in vite.config.ts from CorinneRedirectPage to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CorinneRedirectPage/', // ensures all asset URLs start with /CorinneRedirectPage/
+  base: '/', // ensures all asset URLs start with /CorinneRedirectPage/
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs', // write your built site into “docs/”
